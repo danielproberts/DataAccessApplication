fa506aad680fbf33133ecf6ffb2c5411c3670cce
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2020-09-06 12:13:43 -0400

    initial commit:

---
+++ DataAccessApplication/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DataAccessApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lblStatusBar = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblStatusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(862, 48);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lblStatusBar
+            // 
+            this.lblStatusBar.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.lblStatusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.lblStatusBar.Location = new System.Drawing.Point(0, 848);
+            this.lblStatusBar.Name = "lblStatusBar";
+            this.lblStatusBar.Size = new System.Drawing.Size(862, 38);
+            this.lblStatusBar.TabIndex = 4;
+            this.lblStatusBar.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 28);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 886);
+            this.Controls.Add(this.lblStatusBar);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.lblStatusBar.ResumeLayout(false);
+            this.lblStatusBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip lblStatusBar;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+    }
+}
+
---
+++ DataAccessApplication/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataAccessApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection();
+
+                var datasource = @"DANIELROBERFA92\SQLEXPRESS";//your server
+                var database = "Northwind"; //your database name
+                var username = "droberts";
+                var password = "northwind";
+
+                //your connection string 
+                string connString = @"Data Source=" + datasource + ";Initial Catalog="
+                    + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+
+                connection.ConnectionString = connString;
+                connection.Open();
+
+                lblStatusBar.Text = "Connection Successful";
+                lblStatus.Text = "Connection Successful";
+
+                connection.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error, " +ex);
+            }
+        }
+    }
+}
